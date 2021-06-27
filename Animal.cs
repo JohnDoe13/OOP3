93ee656ce893c2917c3bf4fba24467c239abde77
JohnDoe13 <nick.alzen@live.com>
2021-06-27 23:54:01 +0200

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using OOP3;
+
+namespace OOP3
+{
+    class Animal
+        
+    {
+        
+        public string name;
+        public double weight;
+        public int age;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public void DoSound()
+        {
+            Console.WriteLine("Making lot of sound.");
+        }
+        public Animal(string animalName, double animalWeight, int animalAge)
+        {
+            name = animalName;
+            weight = animalWeight;
+            age = animalAge;
+        }
+        
+     }
+    class Horse : Animal
+    {
+        public int = 23;
+    }
+    
+    
+       
+ } 
+
+        
+
+        //Horse , Dog , Hedgehog , Worm och Bird, Wolf .
+
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace OOP3
+{
+    //Som synes så är jag långt ifrån klar med denna uppgift.
+    //Förstår principen i stora drag men att peta in de olika delarna/byggstenarna är 
+    //en utmaning. Jag vill/kommer fortsätta jobba med denna.
+    //Ni får den här tillsvidare. Hav förtröstan.
+    class Program
+    {
+        public int input_age;
+        //Ja, jag kommer åt variablerna.
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Person p = new Person();
+
+            try
+            {
+
+                Console.WriteLine("Please enter your age:");
+                p.Age = int.Parse(Console.ReadLine());
+            }
+            catch (System.ArgumentException e)
+            {
+                if (p.Age <= 0) 
+                {
+                    //throw new ArgumentException("Age must be greater than 0 <zero");
+                    Console.WriteLine("Age must be greater than 0 <zero>", e);
+                    //Console.WriteLine(e);
+                }
+            }
+
+            try
+            {
+
+                Console.WriteLine("Please enter your Firstname:");
+                p.Fname = Console.ReadLine();
+            }
+            catch (ArgumentException e)
+            {
+                if (p.Fname.Length >=2 || p.Fname.Length<=10)
+                {
+                    //throw new ArgumentException("{0}Firstname must be between 3 and 10 characters.",e);
+                    Console.WriteLine("Age must be greater than 0 <zero>");
+                }
+            }
+            try
+            {
+
+                Console.WriteLine("Please enter your Lastname:");
+                p.Lname = Console.ReadLine();
+            }
+            catch (System.ArgumentException e)
+            {
+                if (p.Lname.Length <= 2 || p.Lname.Length <= 10)
+                {
+                    throw new ArgumentException("{0} Age must be greater than 0 <zero>",e);
+                    //Console.WriteLine("Age must be greater than 0 <zero>");
+                }
+            }
+
+           
+
+           
+        }
+    }
+}
+
